95f4e0cd96de69b3a0c8e9ed618e344de37a01b7
stirante <brzozowski.s.piotr@gmail.com>
2021-06-04 08:13:29 +0200

    Add reverse function

--- src/main/java/com/stirante/json/functions/UtilityFunctions.java
+++ src/main/java/com/stirante/json/functions/UtilityFunctions.java
@@ -4,6 +4,8 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -34,6 +36,12 @@ public class UtilityFunctions {
         return new JSONArray(StreamSupport.stream(arr.spliterator(), false)
                 .map(o -> map.get(o.toString()))
                 .collect(Collectors.toList()));
+    }
+    @JSONFunction
+    private static JSONArray reverse(JSONArray obj) {
+        List<?> l = new ArrayList<>(obj.toList());
+        Collections.reverse(l);
+        return new JSONArray(l);
     }
 
 }
