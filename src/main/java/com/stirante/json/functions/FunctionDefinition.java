7e1737558fbc614734da393d3bc058101c5ca70c
stirante <brzozowski.s.piotr@gmail.com>
2021-06-08 09:30:43 +0200

    Rebuild argument matching to allow for functions with the same number of parameters but different types

--- src/main/java/com/stirante/json/functions/FunctionDefinition.java
+++ src/main/java/com/stirante/json/functions/FunctionDefinition.java
@@ -1,15 +1,31 @@
 package com.stirante.json.functions;
 
 import com.stirante.json.exception.JsonTemplatingException;
-import com.stirante.json.utils.StringUtils;
+import com.stirante.json.utils.Pair;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FunctionDefinition {
+
+    private static final Map<Class<?>, String> NAME_MAP = new HashMap<>();
+
+    static {
+        NAME_MAP.put(JSONObject.class, "<Object>");
+        NAME_MAP.put(JSONArray.class, "<Array>");
+        NAME_MAP.put(Number.class, "<Number>");
+        NAME_MAP.put(Integer.class, "<Integer>");
+        NAME_MAP.put(Double.class, "<Double>");
+        NAME_MAP.put(Float.class, "<Float>");
+        NAME_MAP.put(Boolean.class, "<Boolean>");
+        NAME_MAP.put(String.class, "<String>");
+        NAME_MAP.put(Long.class, "<Long>");
+    }
+
     private final List<Class<?>[]> types = new ArrayList<>();
     private final List<Function<Object[], Object>> implementations = new ArrayList<>();
     private final String name;
@@ -23,47 +39,91 @@ public class FunctionDefinition {
     }
 
     public Object execute(Object[] params, String path) {
-        for (int i = 0, typesSize = types.size(); i < typesSize; i++) {
-            Class<?>[] type = types.get(i);
-            if (params.length == type.length) {
-                paramCheck(params, type, path);
-                try {
-                    return implementations.get(i).apply(params);
-                } catch (JsonTemplatingException ex) {
-                    throw ex.withPath(path);
-                }
+        List<Pair<Integer, Class<?>[]>> sizeMatching = IntStream.range(0, types.size())
+                .filter(value -> params.length == types.get(value).length)
+                .mapToObj(value -> new Pair<>(value, types.get(value)))
+                .collect(Collectors.toList());
+        if (sizeMatching.size() == 0) {
+            throw new JsonTemplatingException(String.format("Incorrect number of parameters passed to function '%s'!", name), path);
+        }
+        List<Pair<Integer, Class<?>[]>> matching =
+                sizeMatching.stream()
+                        .filter(classes -> paramCheck(params, classes.getValue()))
+                        .collect(Collectors.toList());
+        if (matching.size() == 0) {
+            throw new JsonTemplatingException(String.format("Function '%s' got unexpected params. Expected one of %s", name, sizeMatching
+                    .stream()
+                    .map(integerPair -> toString(integerPair.getValue()))
+                    .collect(Collectors.joining(", "))), path);
+        }
+        else if (matching.size() == 1) {
+            Pair<Integer, Class<?>[]> pair = matching.get(0);
+            for (int i = 0, paramsLength = params.length; i < paramsLength; i++) {
+                params[i] = getEmptyIfNull(pair.getValue()[i], params[i]);
+            }
+            try {
+                return implementations.get(pair.getKey()).apply(params);
+            } catch (JsonTemplatingException ex) {
+                throw ex.withPath(path);
             }
         }
-        throw new JsonTemplatingException(String.format("Incorrect number of parameters passed to function '%s'!", name), path);
+        else {
+            throw new JsonTemplatingException(String.format("Ambiguous function call to '%s'. Following variants matched: %s", name, matching
+                    .stream()
+                    .map(integerPair -> toString(integerPair.getValue()))
+                    .collect(Collectors.joining(", "))), path);
+        }
     }
 
-    private void paramCheck(Object[] params, Class<?>[] types, String path) {
+    private String toString(Class<?>[] classes) {
+        return name + "(" + Arrays.stream(classes).map(NAME_MAP::get).collect(Collectors.joining(", ")) + ")";
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T getEmptyIfNull(Class<T> cls, Object param) {
+        if (!cls.isInstance(param)) {
+            if (cls == JSONArray.class) {
+                return (T) new JSONArray();
+            }
+            else if (cls == Integer.class) {
+                return (T) Integer.valueOf(0);
+            }
+            else if (cls == Double.class) {
+                return (T) Double.valueOf(0);
+            }
+            else if (cls == Float.class) {
+                return (T) Float.valueOf(0);
+            }
+            else if (cls == Long.class) {
+                return (T) Long.valueOf(0);
+            }
+            else if (cls == Number.class) {
+                return (T) Integer.valueOf(0);
+            }
+            else if (cls == JSONObject.class) {
+                return (T) new JSONObject();
+            }
+            else if (cls == String.class) {
+                return (T) "";
+            }
+            else if (cls == Boolean.class) {
+                return (T) Boolean.FALSE;
+            }
+            throw new IllegalArgumentException("Unexpected type: " + cls.getName());
+        }
+        else {
+            return (T) param;
+        }
+    }
+
+    private boolean paramCheck(Object[] params, Class<?>[] types) {
         for (int i = 0; i < params.length; i++) {
             Object param = params[i];
-            if (!types[i].isInstance(param)) {
-                if (param == null) {
-                    if (types[i] == JSONArray.class) {
-                        params[i] = new JSONArray();
-                        return;
-                    }
-                    if (types[i] == Integer.class) {
-                        params[i] = 0;
-                        return;
-                    }
-                    if (types[i] == JSONObject.class) {
-                        params[i] = new JSONObject();
-                        return;
-                    }
-                    if (types[i] == String.class) {
-                        params[i] = "";
-                        return;
-                    }
-                }
-                throw new JsonTemplatingException(String.format("Function '%s' expected %s as %s parameter, but got %s", name, types[i]
-                                .getTypeName(), StringUtils.getFormatterNumber(i + 1),
-                        param == null ? "null" : param.getClass()), path);
+            if (!types[i].isInstance(param) && param != null) {
+                return false;
             }
         }
+        return true;
     }
 
     public void addImplementation(Function<Object[], Object> implementation, Class<?>... types) {
